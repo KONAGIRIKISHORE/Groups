72a3d339fd67e223277b3d4dda6b539b0e6b8ec7
jagadeesh82001 <145035048+jagadeesh82001@users.noreply.github.com>
2023-10-07 22:17:51 +0530

    Add files via upload

---
+++ students/src/main/java/com/students/entity/student.java
@@ -0,0 +1,30 @@
+package com.students.entity;
+
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Data
+@NoArgsConstructor
+
+@Entity
+public class student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    // Constructors, getters, setters, and other methods
+
+    // You would define constructors, getters, setters, and other methods here.
+    // Additionally, you can annotate fields with JPA annotations to configure
+    // their mapping to database columns.
+}
